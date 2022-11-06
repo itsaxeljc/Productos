9bc843b5e7c725e9516d6281358b1362cb853f6a
Axel Jacobo <83792830+itsaxeljc@users.noreply.github.com>
2022-11-06 13:41:25 -0700

    Añadir nuevo producto completado
    Creo que ya quedó todo lo del controlador con lo que pide el profe, ya solo sería detallar la Interfaz para que se vea más chida. Le agregué el dialog para agregar nuevo producto, ya lo hace todo bien al parecer

--- Productos/src/app/home/home.page.ts
+++ Productos/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Producto } from "../models/producto";
 import { ProductoService } from "../services/producto.service";
+import { AlertController } from '@ionic/angular';
 
 //Routes
 import { Router } from '@angular/router'; 
@@ -15,7 +16,8 @@ export class HomePage {
   public productos: Producto[];
   public producto: Producto;
 
-  constructor(private productoService: ProductoService, private router: Router) {
+  constructor(private productoService: ProductoService, private router: Router,
+    private alertController: AlertController) {
     this.productos =  this.productoService.getProductos();
   }
 
@@ -44,4 +46,49 @@ export class HomePage {
     this.productoService.addProductoCarrito(producto);
   }
 
+  async alertAddProduct() {
+    const alert = await this.alertController.create({
+      header: 'Ingrese la información de su producto',
+      inputs: [
+        {
+          name: 'description',
+          type: 'text',
+          placeholder: 'Descripción',
+        },
+        {
+          name: 'foto',
+          type: 'text',
+          placeholder: 'URL de la imagen'
+        },
+        {
+          name: 'precio',
+          type: 'number',
+          placeholder: 'Precio'
+        },
+      ],
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () => {
+          console.log('Confirm Cancel');
+        }
+      },
+      {
+        text: 'Aceptar',
+        handler: (alertData) => {
+          let i = this.productos.length+1;
+          this.producto = {
+            foto: alertData.foto,
+            description: alertData.description,
+            precio: parseFloat(alertData.precio),
+            id: i.toString(),
+          }
+          this.productoService.addProducto(this.producto)
+        }
+      }],
+    });
+
+    await alert.present();
+  }
 }
